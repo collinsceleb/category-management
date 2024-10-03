dd69958509cd4bf9f399c9f5cb702da8f5f4a772
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2024-10-03 21:59:41 +0100

    Set swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('Category Management')
+    .setDescription('API for Category Management')
+    .setExternalDoc('Postman Collection', '/docs-json')
+    .setVersion('1.0')
+    .addTag('category')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
